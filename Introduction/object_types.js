1835cbce01c64d69889770823ffdceece0fd0472
GenuinelyMeizner <frederikmpetersen@gmail.com>
2022-11-23 06:43:25 +0100

    Introduction finished

---
+++ Introduction/object_types.js
@@ -0,0 +1,7 @@
+function functionExample () {
+    return
+}
+console.log(typeof(functionExample))
+console.log(typeof []);
+console.log(typeof new Date());
+console.log(typeof Error("This is an error"));
---
+++ Introduction/primitive_types.js
@@ -0,0 +1,8 @@
+console.log(typeof 1);
+console.log(typeof BigInt(Number.MAX_SAFE_INTEGER));
+console.log(typeof "This is a string");
+console.log(typeof true);
+console.log(typeof Symbol("This is a symbol"));
+console.log(typeof undefined);
+const nullExample = null;
+console.log(typeof(nullExample));
